d00f0712f2b20b9aee0722019cfebf8857eff7dd
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-09-21 09:04:56 +0530

    to check character

---
+++ to check character.py
@@ -0,0 +1,10 @@
+ch=input("enter the character")
+
+if ch>='a' and ch<='z' or ch>='A' and ch<='Z':
+    print("the given character",ch,"is aplabatic character")
+elif ch>='0' and ch<='9':
+    print("the given character ",ch, "is digit")
+else:
+    print("the given character",ch, " is special character")
+
+ 
